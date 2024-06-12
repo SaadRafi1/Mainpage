4d78aea709675eea55b157a1ccf46ba0d73f98c8
Saad Rafi <saadrafi990@gmail.com>
2024-06-12 12:37:14 +0500

    feat: implement list and card screen

---
+++ Mainpage/Customtableviewcell.swift
@@ -0,0 +1,20 @@
+//
+//  Customtableviewcell.swift
+//  Mainpage
+//
+//  Created by o9tech on 11/06/2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var iconImageview: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var currency: UILabel!
+    @IBOutlet weak var like: UIButton!
+    @IBOutlet weak var buycart: UIButton!
+
+}
+
+
